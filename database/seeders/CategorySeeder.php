3cdf1bce3626d770ea01379a165f1dbdbe45df74
Ricardo Mex <ricardo.mexan@gmail.com>
2022-11-05 11:50:14 -0600

    Seeders y factories para Category

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use Database\Factories\CategoryFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
@@ -14,6 +17,36 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = [
+            [
+                'name'=>'Celulares y tablets',
+                'slug'=>Str::slug('Celulares y tablets'),
+                'icon'=>'<li class="fas fa-mobile-alt"></li>',
+            ],
+            [
+                'name'=>'TV, audio, video',
+                'slug'=>Str::slug('TV, audio, video'),
+                'icon'=>'<li class="fas fa-tv"></li>',
+            ],
+            [
+                'name'=>'consola y video juegos',
+                'slug'=>Str::slug('Consola y video juegos'),
+                'icon'=>'<li class="fas fa-gamepad"></li>',
+            ],
+            [
+                'name'=>'Computacion',
+                'slug'=>Str::slug('Computacion'),
+                'icon'=>'<li class="fas fa-laptop"></li>',
+            ],
+            [
+                'name'=>'Moda',
+                'slug'=>Str::slug('Moda'),
+                'icon'=>'<li class="fas fa-tshirt"></li>',
+            ],
+        ];
+
+        foreach ($categories as $category ) {
+            Category::factory()->create($category);
+        }
     }
 }
