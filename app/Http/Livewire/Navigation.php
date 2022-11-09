e892d8e21d1c7c71b1e6cdabe6dc7cf43398d3e8
Ricardo Mex <ricardo.mexan@gmail.com>
2022-11-08 23:06:49 -0600

    nanbar

--- app/Http/Livewire/Navigation.php
+++ app/Http/Livewire/Navigation.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Http\Livewire;
-
+use App\Models\Category;
 use Livewire\Component;
 
 class Navigation extends Component
 {
     public function render()
     {
-        return view('livewire.navigation');
+        $categories =  Category::all();
+        return view('livewire.navigation', compact('categories'));
     }
 }
